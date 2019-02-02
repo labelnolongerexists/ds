ddc29cc22da80e6a89ec351f429295389d25e4a0
污墩: 习近平新时代中国特色社会主义思想，是对马克思列宁主义、毛泽东思想、邓小平理论、三个代表重要思想、科学发展观的继承和发展，是马克思主义中国化最新成果，是党和人民实践经验和集体智慧的结晶，是中国特色社会主义理论体系的重要组成部分，是全党全国人民为实现中华民族伟大复兴而奋斗的行动指南，必须长期坚持并不断发展。 <zijing.wu@qyer.com>
2019-02-02 19:13:42 +0800

    Heap

---
+++ ds-main/src/main/java/com/hhrb/ds/learn/heap/Heap.java
@@ -0,0 +1,120 @@
+package com.hhrb.ds.learn.heap;
+
+import com.google.common.collect.Lists;
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.List;
+import java.util.Random;
+
+/**
+ * User: Z J Wu Date: 2019-02-02 Time: 09:58 Package: com.hhrb.ds.learn.heap
+ */
+public class Heap<T extends Comparable<T>> {
+
+  private List<T> store;
+
+  public Heap() {
+    store = Lists.newArrayList();
+  }
+
+  public int size() {
+    return CollectionUtils.size(store);
+  }
+
+  private T max(T t1, T t2) {
+    return gt(t1, t2) ? t1 : t2;
+  }
+
+  private int indexOfMax(int idx1, int idx2) {
+    return gt(get(idx1), get(idx2)) ? idx1 : idx2;
+  }
+
+  private boolean gt(T t1, T t2) {
+    return t1.compareTo(t2) == 1;
+  }
+
+  public T get(int idx) {
+    return store.get(idx);
+  }
+
+  public Heap(List<T> raw) {
+    store = Lists.newArrayList(raw);
+    for (int i = (size() - 1) / 2; i >= 0; i--) {
+      int currentIdx = i;
+      for (; ; ) {
+        int leftChildIdx = getLeftChildIdx(currentIdx);
+        if (leftChildIdx < 0) {
+          break;
+        }
+        int rightChildIdx = getRightChildIdx(currentIdx), maxNodeIdx;
+        if (rightChildIdx > 0) {
+          maxNodeIdx = indexOfMax(currentIdx, indexOfMax(leftChildIdx, rightChildIdx));
+        } else {
+          maxNodeIdx = indexOfMax(currentIdx, leftChildIdx);
+        }
+        if (currentIdx == maxNodeIdx) {
+          break;
+        }
+        swap(currentIdx, maxNodeIdx);
+        currentIdx = maxNodeIdx;
+      }
+    }
+  }
+
+  private boolean hasChildren(int idx) {
+    return getLeftChildIdx(idx) > 0;
+  }
+
+  public int getParent(int idx) {
+    if (idx <= 0 || idx >= store.size()) {
+      return -1;
+    }
+    return idx / 2;
+  }
+
+  public T getLeftChild(int idx) {
+    return get(getLeftChildIdx(idx));
+  }
+
+  public int getLeftChildIdx(int idx) {
+    if (idx < 0 || idx >= store.size() || (idx * 2 + 1) >= store.size()) {
+      return -1;
+    }
+    return idx * 2 + 1;
+  }
+
+  public T getRightChild(int idx) {
+    return get(getRightChildIdx(idx));
+  }
+
+  public int getRightChildIdx(int idx) {
+    if (idx < 0 || idx > store.size() || (idx * 2 + 2) >= store.size()) {
+      return -1;
+    }
+    return idx * 2 + 2;
+  }
+
+  private void swap(int i, int j) {
+    T t = store.get(i);
+    store.set(i, store.get(j));
+    store.set(j, t);
+  }
+
+  public static void main(String[] args) {
+    Random random = new Random();
+
+    int elementCnt = 10;
+    List<Integer> list = Lists.newArrayListWithCapacity(elementCnt);
+
+    for (int i = 0; i < elementCnt; i++) {
+      list.add(random.nextInt(100));
+    }
+    //    list.add(1);
+    //    list.add(2);
+    //    list.add(3);
+    System.out.println(list);
+    Heap<Integer> heap = new Heap<>(list);
+    System.out.println(heap.store);
+  }
+
+}
